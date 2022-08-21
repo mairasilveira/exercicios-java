83169996c0bb3b34fca30f01bc9eac8b3a5fb05f
Maira Silveira <silveirasmaira@gmail.com>
2022-08-21 12:33:19 -0300

    conversor de dollar pra real

---
+++ converter/converter/src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import util.CurrencyConverter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What's the dollar price? ");
+        double dollarPrice = sc.nextDouble();
+        System.out.print("How many dollars will be bought? ");
+        double amount = sc.nextDouble();
+        double result = CurrencyConverter.dollarToReal(dollarPrice, amount);
+        System.out.printf("Amount to be paid in reais: %.2f", result);
+
+        sc.close();
+    }
+}
