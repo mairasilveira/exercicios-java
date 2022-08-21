0269391ee1602aba5153ece6aea0600ef2985291
Maira Silveira <silveirasmaira@gmail.com>
2022-08-21 14:48:53 -0300

    construtor product

--- product/Product/src/application/Program.java
+++ product/Product/src/application/Program.java
@@ -10,8 +10,6 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
         product.name = sc.next();
@@ -19,6 +17,7 @@ public class Program {
         product.price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
+        Product product = new Product(name, price, quantity);
 
         System.out.println(product);
 
